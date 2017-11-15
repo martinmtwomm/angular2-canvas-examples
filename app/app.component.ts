58f4dae2d82577b5eac80c66285010b5cbece494
martinm <martinmtwo@gmail.com>
2017-11-15 20:12:50 +0100

    removed unuesed parts identified rectangles are now drawn on transparent canvas over invoice (image) added percentage calculation to adapt rectangle size on image size (hardcoded example)

--- app/app.component.ts
+++ app/app.component.ts
@@ -21,46 +21,11 @@ import {rect} from "./interfaces";
             margin-bottom: 0;
         }
     `],
-    template: `
-  <h1>Charts of important things</h1>
-  <h3>This barchart component uses an attribute directive on a canvas element. Meh.</h3>
-  <barchart [data]="salesData" [width]="1800" [height]="1000" [colors]="['navy']" [title]="'Monthly Sales 2016'"></barchart>
+    template: `        
+  <barchart [data]="salesData" [width]="1024" [height]="1000" [colors]="['navy']" [title]="'Monthly Sales 2016'"></barchart>
 `
 })
 export class AppComponent {
-    salesData = [
-        { label: 'JAN', value: this.getRandomInt() },
-        { label: 'FEB', value: this.getRandomInt() },
-        { label: 'MAR', value: this.getRandomInt() },
-        { label: 'APR', value: this.getRandomInt() },
-        { label: 'JUN', value: this.getRandomInt() },
-        { label: 'JUL', value: this.getRandomInt() }
-    ] ;
-
-    consistencyData = [
-        { label: 'JAN', value: 83 },
-        { label: 'FEB', value: 41 },
-        { label: 'MAR', value: 10 },
-        { label: 'APR', value: 98 },
-        { label: 'JUN', value: 50 },
-        { label: 'JUL', value: 25 },
-        { label: 'AUG', value: 122 },
-        { label: 'SEP', value: 36 },
-        { label: 'OCT', value: 100 },
-    ] ;
-    colors2 = [ '#5882FA', 'orange', '#0B3B2E', 'red','#689a68', '#FF00BF', '#008080', '#f16e23'  ];
-
-    pizzaData = [
-        { label: 'Pepperoni', value: this.getRandomInt() },
-        { label: 'Sausage', value: this.getRandomInt() },
-        { label: 'Onions', value:  this.getRandomInt() },
-        { label: 'Olives', value:  this.getRandomInt() },
-        { label: 'Pineapple', value:  this.getRandomInt() }
-    ] ;
-
-    getRandomInt(max = 100) {
-        return Math.floor(Math.random()*max);
-    }
     data: any = null;
 
     constructor(private _http: Http) {
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from "./app.component";
-import {BarchartComponent, BarchartDataDirective} from "./barchart.component";
+import {BarchartComponent, BarchartDataDirective} from "./invoice-manager-canvas.component";
 import {SmileyDirective} from "./smiley.component";
 import {PiechartComponent} from "./piechart.component";
 import {Barchart2Component} from "./barchart2.component";
